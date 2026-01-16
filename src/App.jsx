271f522f05b4d0e42afffc55f18c44e7393478db
leoliveirak <klitzkel789@gmail.com>
2026-01-16 19:59:17 -0300

    feat: implementa depoimento dos saBÔRes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero.jsx'
 import Marquee from './components/Marquee.jsx'
 import About from './sections/About.jsx'
 import Products from './sections/Products.jsx'
+import Testimonials from './sections/Testimonials.jsx'
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
        <About/>
 
        <Products/>
+
+       <Testimonials/>
     </div>
   )
 }
